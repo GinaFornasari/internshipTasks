eae98da6820aa1d78b51f5eaffd9550627b77feb
GinaFornasari <152594743+GinaFornasari@users.noreply.github.com>
2023-12-17 20:56:49 +0200

    Error handling done
    Need to change to handle doubles, only works for integers at the moment

---
+++ SimpleCalculatorWinforms/SimpleCalculatorWinforms/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace SimpleCalculatorWinforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblExpression = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.textboxAnswer = new System.Windows.Forms.TextBox();
+            this.btnOpenB = new System.Windows.Forms.Button();
+            this.btnCloseB = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblExpression
+            // 
+            this.lblExpression.AutoSize = true;
+            this.lblExpression.Location = new System.Drawing.Point(300, 23);
+            this.lblExpression.Name = "lblExpression";
+            this.lblExpression.Size = new System.Drawing.Size(190, 20);
+            this.lblExpression.TabIndex = 0;
+            this.lblExpression.Text = "Enter a simple expression";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(145, 76);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 60);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(245, 76);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 60);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(350, 76);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 60);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(145, 140);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 60);
+            this.btn4.TabIndex = 2;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(245, 140);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 60);
+            this.btn5.TabIndex = 2;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(350, 140);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 60);
+            this.btn6.TabIndex = 2;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(145, 201);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 60);
+            this.btn7.TabIndex = 2;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(245, 201);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 60);
+            this.btn8.TabIndex = 2;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(350, 201);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 60);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(245, 261);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 60);
+            this.btn0.TabIndex = 2;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(516, 76);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 60);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(516, 140);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 60);
+            this.btnMinus.TabIndex = 2;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(516, 201);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 60);
+            this.btnMultiply.TabIndex = 2;
+            this.btnMultiply.Text = "x";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(516, 261);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 60);
+            this.btnDivide.TabIndex = 2;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(282, 398);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(92, 40);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // textboxAnswer
+            // 
+            this.textboxAnswer.Enabled = false;
+            this.textboxAnswer.Location = new System.Drawing.Point(145, 346);
+            this.textboxAnswer.Name = "textboxAnswer";
+            this.textboxAnswer.Size = new System.Drawing.Size(446, 26);
+            this.textboxAnswer.TabIndex = 4;
+            // 
+            // btnOpenB
+            // 
+            this.btnOpenB.Location = new System.Drawing.Point(621, 135);
+            this.btnOpenB.Name = "btnOpenB";
+            this.btnOpenB.Size = new System.Drawing.Size(75, 60);
+            this.btnOpenB.TabIndex = 2;
+            this.btnOpenB.Text = "(";
+            this.btnOpenB.UseVisualStyleBackColor = true;
+            // 
+            // btnCloseB
+            // 
+            this.btnCloseB.Location = new System.Drawing.Point(621, 201);
+            this.btnCloseB.Name = "btnCloseB";
+            this.btnCloseB.Size = new System.Drawing.Size(75, 60);
+            this.btnCloseB.TabIndex = 2;
+            this.btnCloseB.Text = ")";
+            this.btnCloseB.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(400, 398);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(106, 40);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.textboxAnswer);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnCloseB);
+            this.Controls.Add(this.btnOpenB);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.lblExpression);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblExpression;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox textboxAnswer;
+        private System.Windows.Forms.Button btnOpenB;
+        private System.Windows.Forms.Button btnCloseB;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ SimpleCalculatorWinforms/SimpleCalculatorWinforms/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculatorWinforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //btnMinus.Enabled = false;
+            btnPlus.Enabled = false;
+            btnDivide.Enabled = false;
+            btnMultiply.Enabled = false;
+
+            btn1.Click += CommonButtonClick;
+            btn2.Click += CommonButtonClick;
+            btn3.Click += CommonButtonClick;
+            btn4.Click += CommonButtonClick;
+            btn5.Click += CommonButtonClick;
+            btn6.Click += CommonButtonClick;
+            btn7.Click += CommonButtonClick;
+            btn8.Click += CommonButtonClick;
+            btn9.Click += CommonButtonClick;
+            btn0.Click += CommonButtonClick;
+            btnMinus.Click += OperationButtonClick;
+            btnMultiply.Click += OperationButtonClick;
+            btnDivide.Click += OperationButtonClick;
+            btnPlus.Click += OperationButtonClick;
+            btnOpenB.Click += BracketButtonClick;
+            btnCloseB.Click += BracketButtonClick;
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void CommonButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+          
+            if (clickedButton != null)
+            {
+                textboxAnswer.AppendText(clickedButton.Text);
+                btnMinus.Enabled = true;
+                btnPlus.Enabled = true;
+                btnDivide.Enabled = true;
+                btnMultiply.Enabled = true;
+            }
+        }
+        private void OperationButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if (clickedButton != null)
+            {
+                textboxAnswer.AppendText(clickedButton.Text);
+                //btnMinus.Enabled = false;
+                btnPlus.Enabled = false;
+                btnDivide.Enabled = false;
+                btnMultiply.Enabled = false;
+            }
+        }
+
+        private void BracketButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+
+            if (clickedButton != null)
+            {
+                textboxAnswer.AppendText(clickedButton.Text);
+            }
+        }
+
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string express = textboxAnswer.Text;
+            express = express.Replace('x', '*');
+            textboxAnswer.Text = (getSolution(express)).ToString();
+            if (textboxAnswer.Text.Equals("")){
+                //btnMinus.Enabled = false;
+                btnPlus.Enabled = false;
+                btnDivide.Enabled = false;
+                btnMultiply.Enabled = false;
+            }
+        }
+        public string getSolution(string express)
+        {
+            //handle brackets 
+            List<int> pointerOpen = new List<int>();
+
+            for (int i = 0; i < express.Length; i++)
+            { 
+                if (express[i].Equals('('))
+                {
+                    if ((i >= 1) && (Char.IsDigit(express[i-1])))
+                    {
+                        MessageBox.Show("Incorrectly formatted, missing operations");
+                        return "";
+                    }
+                    pointerOpen.Add(i);
+                }
+                if (express[i].Equals(')'))
+                {
+                    if (i < express.Length - 1 && Char.IsDigit(express[i+1]))
+                    {
+                        MessageBox.Show("Incorrectly formatted, missing operations");
+                        return "";
+                    }
+                    if (pointerOpen.Count() == 0)
+                    {
+                        MessageBox.Show("Incorrectly formatted, missing brackets");
+                        return "";
+                    }
+                    else
+                    {
+                        string temp = express.Substring(pointerOpen.Last() + 1, i - pointerOpen.Last() - 1);
+
+                        //send this off as an expression
+                        
+                        var ans = calculate(temp);
+                        int answer = 0; 
+                        if (ans.Equals(""))
+                        {
+                            return ""; 
+                        }
+                        else
+                        {
+                            answer = int.Parse(ans); 
+                        }
+
+                        //replace expression with answer and change i appropriately 
+                        express = express.Substring(0, pointerOpen.Last()) + answer + express.Substring(i + 1);
+                        i = pointerOpen.Last() - 1;
+                        pointerOpen.RemoveAt(pointerOpen.Count - 1);
+                    }
+                }
+            }
+            if (pointerOpen.Count() != 0)
+            {
+                MessageBox.Show("Incorrectly formatted, missing brackets");
+            }
+            else
+            {
+                return (calculate(express).ToString());
+            }
+            //all bracketed terms gone, calc the leftovers
+
+            return ""; 
+
+        }
+     
+
+        public string calculate(string express)
+        {
+            char[] delimiterChars = {
+                            '*',
+                            '/',
+                            '+',
+                            '-'
+                            };
+            var numbers = express.Split(delimiterChars).ToList();
+            numbers.RemoveAll(s => string.IsNullOrWhiteSpace(s));
+            express = checkForNegative(numbers, express);
+
+            if (express.IndexOf('*') != -1 || express.IndexOf('/') != -1)
+            {
+                numbers = higherOrder(numbers, express);
+            }
+
+            if (numbers == null)
+            {
+                return ""; 
+            }
+
+            int count = 0;
+            for (int i = 0; i < express.Length; i++)
+            {
+                if (express[i].Equals('+'))
+                {
+                    int temp = int.Parse(numbers[count]) + int.Parse(numbers[++count]);
+                    numbers[count] = "" + temp;
+                }
+                else if (express[i].Equals('-'))
+                {
+                    int temp;
+                    //on minus: previous can be an operation, following has to be a number
+                    if (!int.TryParse(numbers[count], out temp))
+                    {
+                        //if the previous is not a number
+                        MessageBox.Show("made it this far"); 
+
+                    }
+
+                    temp = int.Parse(numbers[count]) - int.Parse(numbers[++count]);
+                    numbers[count] = "" + temp;
+                }
+            }
+            return numbers[count];
+        }
+         public static string Show(List<string> nums)
+         {
+             string str = "";
+            for(int i=0; i<nums.Count; i++)
+            {
+                str += nums[i] + "("+ i+")";
+            }
+             
+             return str;
+         }
+        public static string checkForNegative(List<string> numbers, string express)
+        {
+            //MessageBox.Show(Show(numbers));
+            int count = 0; 
+            for (int i = 0; i < express.Length; i++)
+            {
+                if (Char.IsDigit(express[i]))
+                {
+                    count++;
+                }
+
+                if (express[i].Equals('-'))
+                {
+                    if (i == 0)
+                    {
+                        int temp = int.Parse(numbers[0]) *-1;
+                        numbers[0] = temp.ToString();
+                        express = express.Substring(0, i) + express.Substring(i + 1);
+                        i = i - 1;
+                       // MessageBox.Show("new " + express); 
+
+                    }
+                    if (i>0 && !Char.IsDigit(express[i - 1]))
+                    {
+                        if(express[i - 1].Equals("-")){
+                            MessageBox.Show("double minus");
+                            express = express.Substring(0, i-1) + "+"+ express.Substring(i + 2);
+                            i = i - 1;
+                        }
+                        else
+                        {
+                            int temp = int.Parse(numbers[count]) * -1;
+                            numbers[count] = temp.ToString();
+                            express = express.Substring(0, i) + express.Substring(i + 1);
+                            i = i - 1;
+                        }
+                        
+                    }
+                }
+            }
+            return express; 
+        }
+
+        public static List<string> higherOrder(List<string> numbers, string express)
+        {
+            int count = 0;
+            for (int i = 0; i < express.Length; i++)
+            {
+
+                if (express[i].Equals('*'))
+                {
+                    int temp = int.Parse(numbers[count]) * int.Parse(numbers[++count]);
+                    numbers[count] = "" + temp;
+                    numbers.RemoveAt(count - 1);
+                    --count;
+                }
+                if (express[i].Equals('/'))
+                {
+                    if (int.Parse(numbers[count+1]) == 0){
+                        MessageBox.Show("Cannot Divide by zero");
+                        return null;
+                    }
+                    int temp = int.Parse(numbers[count]) / int.Parse(numbers[++count]);
+                    numbers[count] = "" + temp;
+                    numbers.RemoveAt(count - 1);
+                    --count;
+                }
+
+                if (express[i].Equals('-') || express[i].Equals('+'))
+                {
+                    count++;
+                }
+            }
+            return numbers;
+        }
+
+        private void btnClear_Click_1(object sender, EventArgs e)
+        {
+            textboxAnswer.Text = String.Empty;
+           // btnMinus.Enabled = false;
+            btnPlus.Enabled = false;
+            btnDivide.Enabled = false;
+            btnMultiply.Enabled = false;
+        }
+    }
+}
